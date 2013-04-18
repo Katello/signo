0edda624a31b7aece035b3cdb3232c644cb58b6e
Marek Hulan <ares@igloonet.cz>
2013-04-18 10:24:32 +0200

    Better I18n support

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,4 +1,10 @@
 # config/initializers/fast_gettext.rb
-FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
-#FastGettext.default_available_locales = ['en','de'] #all you want to allow
-FastGettext.default_text_domain = 'app'
+
+locale_dir = File.join(File.dirname(__FILE__), '..', '..', 'locale')
+default_available_locales = Rails.env.test? ? [] : Dir.entries(locale_dir).reject {|d| d =~ /(^\.|pot$)/ }
+
+# no need to generate MO files for development mode
+locale_type = Rails.env.development? ? :po : :mo
+FastGettext.add_text_domain 'signo', :path => 'locale', :type => locale_type
+FastGettext.default_available_locales = ['en'] + default_available_locales
+FastGettext.default_text_domain = 'signo'
